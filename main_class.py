a0bec0f5652cbf863825883af5b4b26068a6461e
UDQD <qwed125@yandex.ru>
2021-10-12 01:27:05 +0300

    func censor

--- main_class.py
+++ main_class.py
@@ -41,3 +41,17 @@ class Mainclass:
         x = rnd(0, len(resp) - 1)
 
         return resp.iloc[x]['Line_2']
+
+    def censor(self, s):
+        bad_words = [self.alf[1] + self.alf[8] + self.alf[19] + self.alf[2] + self.alf[7],
+                     self.alf[5] + self.alf[20] + self.alf[2] + self.alf[10],
+                     self.alf[22] + self.alf[7] + self.alf[14] + self.alf[17] + self.alf[4],
+                     self.alf[0] + self.alf[18] + self.alf[18],
+                     self.alf[13] + self.alf[8] + self.alf[6] + self.alf[6]]
+        for el in bad_words:
+            if el in s:
+                l = len(el)
+                for i in range(len(s) - l + 1):
+                    if s[i:i + l] == el:
+                        s = s[:i + 1] + '*' * (l - 2) + s[i + l - 1:]
+        return s
