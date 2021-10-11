506b466e5acb2032e74889787b89a58fd439fee7
UDQD <qwed125@yandex.ru>
2021-10-12 01:06:20 +0300

    init

--- main_class.py
+++ main_class.py
@@ -2,3 +2,18 @@ import pandas as pd
 from scipy.spatial.distance import cosine
 from random import randint as rnd
 import string
+
+
+
+class Mainclass:
+    def __init__(self):
+        self.targ_vec = []
+        self.sp_df = pd.read_csv('dialog_data.csv')
+        self.alf = string.ascii_lowercase
+        self.di = self.create_di()
+
+    def create_di(self):
+        di = {}
+        for i in range(26):
+            di[self.alf[i]] = i
+        return di
